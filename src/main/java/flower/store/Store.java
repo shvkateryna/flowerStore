fce0120b503a0191a697e6ccc2e624cb9635c282
shvkateryna <116515521+shvkateryna@users.noreply.github.com>
2023-10-12 09:44:25 +0300

    Add files via upload

---
+++ src/main/java/flower/store/Store.java
@@ -0,0 +1,23 @@
+package flower.store;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import flower.store.filter.SearchFilter;
+
+public class Store {
+
+    private List<Item> items;
+
+    public List<Item> search(SearchFilter filter) {
+
+        List<Item> foundItems = new ArrayList<>();
+
+        for (Item item: items) {
+            if (filter.match(item))  {
+                foundItems.add(item);
+            }
+        }
+        return foundItems;
+    }
+}
