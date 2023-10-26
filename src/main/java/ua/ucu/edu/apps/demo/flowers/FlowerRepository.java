7cad4af70dbd2b99d4f6a5dac92fd303d24294ba
Kateryna <shvahuliak.pn@ucu.edu.ua>
2023-10-26 21:31:02 +0300

    changes

--- src/main/java/ua/ucu/edu/apps/demo/flowers/FlowerRepository.java
+++
@@ -1,7 +0,0 @@
-package ua.ucu.edu.apps.demo.flowers;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FlowerRepository extends JpaRepository<Flower, Integer> {
-    
-}
